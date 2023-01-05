2b7c3ea4b524962df19db1cc7d5be55971c5ba4e
Pervin Çalışkan <pervincaliskan01@hotmail.com>
2023-01-05 17:27:00 +0300

    exceptions example added

---
+++ exceptions.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 17:19:57 2023
+
+@author: Pervin
+"""
+
+
+#Hatalar / istisnalar (exceptions)
+
+#ZeroDivisionError hatasi
+a = 10
+b = 0
+
+a/b
+
+
+try:
+    print(a/b)
+except ZeroDivisionError:
+    print("Payda da sifir olmaz")
+
+
+#tip hatasi
+    
+a = 10    
+b = "2"
+
+a / b
+
+try:
+    print(a/b)
+except TypeError:
+    print("Sayi ve string problemi")
+
+
+
+
+a = 10    
+b = 2
+
+a / b
+
+try:
+    print(a/b)
+except TypeError:
+    print("Sayi ve string problemi")
+
+
+
+a = [13, 10, 15, 12, 17, 12, 19, 18, 11, 12, 190]
+a.sort()
+a
+
+
+a
+import numpy as np
+np.median(a)
